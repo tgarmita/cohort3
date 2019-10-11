9871c43a1ebe12ccd36abda11f9341c36e402b80
unknown <tgarmita@gmail.com>
2019-10-11 16:38:34 -0600

    complete syntax files

--- src/javascript/syntax.js
+++ src/javascript/syntax.js
@@ -1,29 +1,29 @@
 // --------------- Copy this section into your code syntax.js as comments --------
 
 // define attributes / variables
-    // number
-    const num = () => typeof(10.5);
+// number
+const num = () => typeof (10.5);
 
-    // string
-    const str = () => typeof("hi");
+// string
+const str = () => typeof ("hi");
 
-    // boolean
-    const bool = () => typeof(false);
+// boolean
+const bool = () => typeof (false);
 
-    // array
-    const arry = () => Array.isArray([1, "two", true]);
-    const arryEle = () => [1, "two", true];
+// array
+const arry = () => Array.isArray([1, "two", true]);
+const arryEle = () => [1, "two", true];
 
-    // dictionary / objects
-    const obj = () => typeof({title: "California Dreamin'", artist: "The Mamas & the Papas"});
-    const objProp = () => ({title: "California Dreamin'", artist: "The Mamas & the Papas"});
-    
-    // undefined
-    const undef = (x) => x;
+// dictionary / objects
+const obj = () => typeof ({ title: "California Dreamin'", artist: "The Mamas & the Papas" });
+const objProp = () => ({ title: "California Dreamin'", artist: "The Mamas & the Papas" });
+
+// undefined
+const undef = (x) => x;
 
 // sample if / else
 const evenOdd = (x) => {
-    if (x % 2 === 0){
+    if (x % 2 === 0) {
         return "even"
     } else {
         return "odd";
@@ -31,42 +31,100 @@ const evenOdd = (x) => {
 };
 
 // functions
-    // parameters
-    const funcPs = (p1,p2,p3) => p3 + (Number(p1) + Number(p2));
-    // returns
-    const funcReturn = (x) => {
-        return x; //exits function after first return
-        return x + 1;
-    };
+// parameters
+const funcPs = (p1, p2, p3) => p3 + (Number(p1) + Number(p2));
+// returns
+const funcReturn = (x) => {
+    return x; //exits function after first return
+    return x + 1;
+};
 
 
 // arrays
-    // add to the front
-    const arrAddFront = (arr, ele) => {
-        arr.unshift(ele);
-        return arr;
-    };
-    // add to the end
-    const arrAddEnd = (arr, ele) => {
-        arr.push(ele);
-        return arr;
-    };
-    // update values
-    const arrUpdate = (arr, newVal, index) => {
-        arr[index] = newVal;
-        return arr;
-    };
+// add to the front
+const arrAddFront = (arr, ele) => {
+    arr.unshift(ele);
+    return arr;
+};
+// add to the end
+const arrAddEnd = (arr, ele) => {
+    arr.push(ele);
+    return arr;
+};
+// update values
+const arrUpdate = (arr, newVal, index) => {
+    arr[index] = newVal;
+    return arr;
+};
 
 // loops 
-    // for
-    // for/in
-    // while
-    // do while
-    // forEach (with array and function)
+// for
+const forLoop = (arr) => {
+    let i;
+    for (i = 1; i < arr.length; i++) {
+    }
+    return i;
+};
+
+// for/in
+const forIn = (obj) => {
+    let x;
+    let asl = "";
+    for (x in obj) {
+        asl += obj[x];
+        if (x != "location") {
+            asl += "/";
+        }
+    }
+    return asl;
+};
+// while
+
+const whileLoop = (arr) => {
+    let i = 0;
+    while (i < arr.length) {
+        i++;
+    }
+    return i;
+};
+
+// do while
+const doWhile = (arr) => {
+    let i = 0;
+    do {
+        i++;
+    }
+    while (i < arr.length);
+    return i;
+};
+
+// forEach (with array and function)
+const forE = (arr) => {
+    const addPunc = (ele, index, arr) => {
+        arr[index] = ele + "!";
+    };
+    arr.forEach(addPunc);
+    return arr;
+};
+
+
 // Objects / Dictionaries
-    // declare object
-    // lookup key to retrieve value
+// declare object
+const decObj = () => {
+    let obj = { name: "Orange", type: "Fruit" };
+    return typeof (obj);
+}
+
+// lookup key to retrieve value
+const objLookup = () => {
+    let obj = { name: "Orange", type: "Fruit" };
+    return obj.type;
+}
 
 // --------------- Copy ends here --------
 
-export {num, str, bool, arry, arryEle, obj, objProp, undef, evenOdd, funcPs, funcReturn, arrAddFront, arrAddEnd, arrUpdate}
+export {
+    num, str, bool, arry, arryEle, obj, objProp, undef, evenOdd, funcPs,
+    funcReturn, arrAddFront, arrAddEnd, arrUpdate, forLoop, forIn, whileLoop,
+    doWhile, forE, decObj, objLookup
+}
--- src/javascript/syntax.test.js
+++ src/javascript/syntax.test.js
@@ -1,89 +1,134 @@
-import * as Functions from './syntax'
+import * as functions from './syntax'
 
 // define attributes / variables
 // number
 test('Test if number', () => {
-    expect(Functions.num()).toBe("number");
+    expect(functions.num()).toBe("number");
 });
 
 
 // string
 test('Test if string', () => {
-    expect(Functions.str()).toBe("string");
+    expect(functions.str()).toBe("string");
 });
 
 
 // boolean
 test('Test if boolean', () => {
-    expect(Functions.bool()).toBe("boolean");
+    expect(functions.bool()).toBe("boolean");
 });
 
 
 // array
 //This test expects true because the function tests an array with Array.isArray()
 test('Test if array', () => {
-    expect(Functions.arry()).toBe(true); 
+    expect(functions.arry()).toBe(true);
 });
 
 test('Test accessing an array element', () => {
-    expect(Functions.arryEle()[1]).toBe("two");
+    expect(functions.arryEle()[1]).toBe("two");
 });
 
 
 // dictionary / objects
 test('Test if object', () => {
-    expect(Functions.obj()).toBe("object");
+    expect(functions.obj()).toBe("object");
 });
 
 test('Test if object has a property', () => {
-    expect(Functions.objProp().title).toBe("California Dreamin'"); //can check with dot notation
-    expect(Functions.objProp()).toHaveProperty("title", "California Dreamin'"); //or with .toHaveProperty
+    expect(functions.objProp().title).toBe("California Dreamin'"); //can check with dot notation
+    expect(functions.objProp()).toHaveProperty("title", "California Dreamin'"); //or with .toHaveProperty
 });
 
 // undefined
 test('Test if undefined', () => {
     let a;
     let b = undefined;
-    function c() {console.log("Should return undefined")};
+    function c() { console.log("Should return undefined") };
 
-    expect(Functions.undef(a)).toBe(undefined);
-    expect(Functions.undef(b)).toBe(undefined);
-    expect(Functions.undef(c())).toBe(undefined);
+    expect(functions.undef(a)).toBe(undefined);
+    expect(functions.undef(b)).toBe(undefined);
+    expect(functions.undef(c())).toBe(undefined);
 });
 
 // sample if / else
 test('Use if/else to test if even or odd number', () => {
-    expect(Functions.evenOdd(2)).toBe("even");
-    expect(Functions.evenOdd(3)).toBe("odd");
+    expect(functions.evenOdd(2)).toBe("even");
+    expect(functions.evenOdd(3)).toBe("odd");
 });
 
 // functions
 // parameters
 test('Test adding parameters', () => {
-    expect(Functions.funcPs(2,2, "Sum of p1 and p2 = ")).toBe("Sum of p1 and p2 = 4");
+    expect(functions.funcPs(2, 2, "Sum of p1 and p2 = ")).toBe("Sum of p1 and p2 = 4");
 });
 
 
 //returns
 test('Test what happens when multiple return statements', () => {
-    expect(Functions.funcReturn(1)).toBe(1);
+    expect(functions.funcReturn(1)).toBe(1);
 });
 
 // arrays
 // add to the front
 test('Test array.unshift()', () => {
-    expect(Functions.arrAddFront(["b","c"], "a")).toStrictEqual(["a","b","c"]);
+    expect(functions.arrAddFront(["b", "c"], "a")).toStrictEqual(["a", "b", "c"]);
 });
 
 // add to the end
 test('Test array.push()', () => {
-    expect(Functions.arrAddEnd(["b","c"], "d")).toStrictEqual(["b","c","d"]);
-});
-// update values
-test('Test updating array values', () => {
-    expect(Functions.arrUpdate(["b","c"], "bee", 0)).toStrictEqual(["bee","c"]);
+    expect(functions.arrAddEnd(["b", "c"], "d")).toStrictEqual(["b", "c", "d"]);
 });
 
+// update values
+test('Test updating array values', () => {
+    expect(functions.arrUpdate(["b", "c"], "bee", 0)).toStrictEqual(["bee", "c"]);
+});
+
+
+// loops 
+// for
+test('Test for loop', () => {
+    expect(functions.forLoop(["a"])).toBe(1);
+    expect(functions.forLoop(["a", "b", "c"])).toBe(3);
+});
+// for/in
+test('Test for/in loop', () => {
+    expect(functions.forIn({ name: "Tyler", age: 33, location: "Calgary" })).toBe("Tyler/33/Calgary");
+});
+
+// while
+test('Test while loop', () => {
+    expect(functions.whileLoop(["a"])).toBe(1);
+    expect(functions.whileLoop(["a", "b", "c"])).toBe(3);
+});
+    
+// do while
+test('Test do while loop', () => {
+    expect(functions.doWhile(["a"])).toBe(1);
+    expect(functions.doWhile(["a", "b", "c"])).toBe(3);
+});
+
+// forEach (with array and function)
+test('Test forEach() method', () => {
+    let a = ["a"];
+    let abc = ["a", "b", "c"]
+    expect(functions.forE(a)).toStrictEqual(["a!"]);
+    expect(functions.forE(abc)).toStrictEqual(["a!","b!","c!"]);
+});
+
+// Objects / Dictionaries
+    // declare object
+    test('Test if declared as object', () => {
+        expect(functions.decObj()).toBe("object");
+    });
+    
+    test('Test object key lookup', () => {
+        expect(functions.objLookup()).toBe("Fruit");
+    });
+
+
+    // lookup key to retrieve value
 
 
 
