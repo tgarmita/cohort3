b8142cc7b42e166efa22250eaf600cd32466b8d2
unknown <tgarmita@gmail.com>
2019-10-18 13:36:00 -0600

    refactor a few functions

--- src/01-getting-started/scripts/functions.js
+++ src/01-getting-started/scripts/functions.js
@@ -6,7 +6,7 @@ const functions = {
         if (num < 10) return "small";
         if (num < 20) return "medium";
         if (num <= 100) return "large";
-        if (num > 100) return "extra large";
+        return "extra large";
     },
 
 
