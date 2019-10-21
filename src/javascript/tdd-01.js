6e84b9953b72f8c5cd4e9103b4dcafaf912aa718
unknown <tgarmita@gmail.com>
2019-10-21 11:54:05 -0600

    update tests

---
+++ src/javascript/tdd-01.js
@@ -0,0 +1,9 @@
+const functions = {
+    emailBuilder: (firstName,lastName) => {
+        return "l.shumlich@evolveu.ca";
+
+    }
+};
+
+
+export default functions;
--- src/javascript/tdd-01.test.js
+++ src/javascript/tdd-01.test.js
@@ -1,3 +1,21 @@
+import functions from "./tdd-01.js";
+
 test('hello world console.log1', () => {
     console.log("Hello World 1");
+});
+
+test('hello world console.log2', () => {
+    console.log("Hello World 2");
+});
+
+test('check a string to see if it is the same', () => {
+    expect('abcd').toEqual('abcde');
+});
+
+test('check an array see if it is the same', () => {
+    expect([1, 2, 3, 4]).toEqual([1, 2, 3, 4, 5]);
+});
+
+test('emailbuilder produces correct format', () => {
+    expect(functions.emailBuilder("larry", "shumlich")).toEqual("l.shumlich@evolveu.ca");
 });
