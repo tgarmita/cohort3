829f8286f6bd124ee55d60975e224ddb31219695
unknown <tgarmita@gmail.com>
2019-10-27 18:02:43 -0600

    build and test account controller

--- src/03-objects/scripts/account.js
+++ src/03-objects/scripts/account.js
@@ -1,21 +1,50 @@
-
 export class Account {
     constructor(name, startingBalance) {
         this.name = name;
         this.currentBalance = startingBalance;
     }
 
-    deposit(depVal) {
-        this.currentBalance += depVal;
+    deposit(value) {
+        this.currentBalance += value;
     }
 
-    withdraw(withDrawVal) {
-        this.currentBalance -= withDrawVal;
+    withdraw(value) {
+        this.currentBalance -= value;
     }
 
-   balance() {
+    balance() {
         return this.currentBalance;
     }
-
 }
 
+
+export class AccountController {
+    //might want to pass "view model" somehow through constructor?
+    constructor() {
+        this.accountList = [];
+    }
+
+    createAccount(name, startingBalance) {
+        this.accountList.push(new Account(name, startingBalance));
+    }
+
+    getAccountList() {
+        return this.accountList;
+    }
+
+    removeAccount(name) {
+        this.accountList = this.accountList.filter(account => account.name !== name);
+    }
+
+    totalAccounts() {
+        return this.accountList.reduce(((accumulator, account) => accumulator + account.currentBalance),0);
+    }
+
+    mostValuableAccount() {
+        return this.accountList.sort((a,b) => b.currentBalance -  a.currentBalance)[0];
+    }
+
+    leastValuableAccount() {
+        return this.accountList.sort((a,b) => a.currentBalance -  b.currentBalance)[0];
+    }
+}
--- src/03-objects/scripts/account.test.js
+++ src/03-objects/scripts/account.test.js
@@ -1,23 +1,63 @@
-import {Account} from './account.js'
+import { Account, AccountController } from './account.js'
 
 
 describe('Account Testing', () => {
     const checkingAccount = new Account("Checking Account", 100);
 
 
-    test('Account properties and methods', () => {
+    test('Account properties', () => {
         expect(checkingAccount.name).toEqual("Checking Account");
         expect(checkingAccount.currentBalance).toEqual(100);
-
-        checkingAccount.deposit(50)
-
-        expect(checkingAccount.currentBalance).toEqual(150);
-
-        checkingAccount.withdraw(25)
-
-        expect(checkingAccount.currentBalance).toEqual(125);
-
-        expect(checkingAccount.balance()).toEqual(125);
     });
 
+    test('deposit(value) adds value to currentBalance', () => {
+        checkingAccount.deposit(50);
+        expect(checkingAccount.currentBalance).toEqual(150);
+    });
+
+    test('withdraw(value) subtracts value from currentBalance', () => {
+        checkingAccount.withdraw(25);
+        expect(checkingAccount.currentBalance).toEqual(125);
+    });
+
+    test('balance() returns currentBalance', () => {
+        expect(checkingAccount.balance()).toEqual(125);
+    });
+});
+
+
+describe('Account Controller Testing', () => {
+    const banker = new AccountController();
+
+
+    test('createAccount() adds new account to accountList', () => {
+        banker.createAccount("Savings Account", 5);
+        expect(banker.accountList[0].name).toEqual("Savings Account");
+    });
+
+    test('getAccountList() returns array of all accounts', () => {
+        banker.createAccount("Checking Account", 75);
+        expect(banker.getAccountList()).toEqual([{ "currentBalance": 5, "name": "Savings Account" }, { "currentBalance": 75, "name": "Checking Account" }]);
+    });
+
+    test('removeAccount() removes account of that name', () => {
+        const accountCount = banker.getAccountList().length
+        banker.removeAccount("Savings Account");
+        expect(banker.getAccountList().length).toEqual(accountCount - 1);
+        expect(banker.getAccountList()).toEqual([{ "currentBalance": 75, "name": "Checking Account" }]);
+    });
+
+    test('totalAccounts() returns total balance of all accounts', () => {
+        banker.createAccount("Joint Account", 500);
+        banker.createAccount("Holiday Account", 25);
+        expect(banker.totalAccounts()).toEqual(600);
+    });
+
+    test('mostValuableAccount() returns account with highest balance', () => {
+        expect(banker.mostValuableAccount()).toEqual({"currentBalance": 500, "name": "Joint Account"});
+    });
+
+    test('leastValuableAccount() returns account with highest balance', () => {
+        expect(banker.leastValuableAccount()).toEqual({"currentBalance": 25, "name": "Holiday Account"});
+    });
 });
