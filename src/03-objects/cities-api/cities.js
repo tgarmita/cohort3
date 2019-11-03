b3ccd47bc55b6501fad8998aa7e409f2cdfb9361
unknown <tgarmita@gmail.com>
2019-11-03 16:22:45 -0700

    complete city, community, and postData methods and tests

--- src/03-objects/cities-api/cities.js
+++ src/03-objects/cities-api/cities.js
@@ -10,12 +10,15 @@ export class City {
     show() {
         return `Key: ${this.key}, Name: ${this.name}, Lat: ${this.lat}, Long: ${this.long}, Population: ${this.pop}`
     }
-    movedIn(value) {
+
+    async movedIn(value) {
         this.pop += value;
+        await postData(url + 'update', this);
     }
 
-    movedOut(value) {
+    async movedOut(value) {
         this.pop -= value;
+        await postData(url + 'update', this);
     }
 
     howBig() {
@@ -28,8 +31,9 @@ export class City {
     }
 }
 
+
 export class Community {
-    constructor(cityList = []) {
+    constructor(cityList) {
         this.cityList = cityList;
     }
 
@@ -37,22 +41,64 @@ export class Community {
         return this.cityList.filter(city => city.key === key)[0];
     }
 
+    async loadData() {
+        let data = await postData(url + 'all');
+        this.cityList = data.map(dataCity => new City(dataCity.key, dataCity.name, dataCity.lat, dataCity.long, dataCity.pop));
+        console.log("Cities loaded:")
+        console.log(this.cityList);
+    }
+
+    async createCity(key, name, lat, long, pop) {
+        const city = new City(key, name, Number(lat), Number(long), Number(pop));
+        this.cityList.push(city);
+        await postData(url + 'add', city);
+    }
+
+    async deleteCity(key) {
+        this.cityList = this.cityList.filter(city => city.key !== key);
+        await postData(url + 'delete', { key });
+    }
+
     whichSphere(city) {
         if (city.lat > 0) return "Northern Hemisphere";
         return "Southern Hemisphere";
     }
 
-    // getPopulation() {
-    //     return this.cityList.reduce(((accumulator, city) => accumulator + Number(city.pop)), 0);
-    // }
+    getPopulation() {
+        return this.cityList.reduce(((accumulator, city) => accumulator + city.pop), 0);
+    }
 
-    // getMostNorthern() {
-    //     return this.cityList.sort((a, b) => b.lat - a.lat)[0];
-    // }
+    getMostNorthern() {
+        return this.cityList.sort((a, b) => b.lat - a.lat)[0];
+    }
 
-    // getMostSouthern() {
-    //     return this.cityList.sort((a, b) => a.lat - b.lat)[0];
-    // }
+    getMostSouthern() {
+        return this.cityList.sort((a, b) => a.lat - b.lat)[0];
+    }
+}
 
 
+const fetch = require('node-fetch');
+const url = 'http://localhost:5000/';
+
+export async function postData(url, data = {}) {
+    // Default options are marked with *
+    const response = await fetch(url, {
+        method: 'POST',     // *GET, POST, PUT, DELETE, etc.
+        mode: 'cors',       // no-cors, *cors, same-origin
+        cache: 'no-cache',  // *default, no-cache, reload, force-cache, only-if-cached
+        credentials: 'same-origin', // include, *same-origin, omit
+        headers: {
+            'Content-Type': 'application/json'
+            // 'Content-Type': 'application/x-www-form-urlencoded',
+        },
+        redirect: 'follow',         // manual, *follow, error
+        referrer: 'no-referrer',    // no-referrer, *client
+        body: JSON.stringify(data)  // body data type must match "Content-Type" header
+    });
+
+    const json = await response.json();    // parses JSON response into native JavaScript objects
+    json.status = response.status;
+    json.statusText = response.statusText;
+    return json;
 }
--- src/03-objects/cities-api/cities.test.js
+++ src/03-objects/cities-api/cities.test.js
@@ -1,55 +1,150 @@
-import {City, Community} from './cities.js'
+import { City, Community, postData } from './cities.js'
 
+const lethbridge = new City(4, "Lethbridge", 49.5, -110, 101500);
+const airdrie = new City(5, "Airdrie", 51.5, -110.1, 70000);
+const sundre = new City(6, "Sundre", 52.5, -110.2, 3000);
+const caroline = new City(7, "Caroline", 53, -110.3, 500);
+const bearberry = new City(8, "Bearberry", 52.7, -110.4, 75);
+const spooky = new City(9, "Spooky", -55, -110.5, 0);
 
 describe('City Testing', () => {
-    const smallCity = new City(4, "Lethbridge", -110.1, 49.5, 101500);
-
 
     test('City properties', () => {
-        expect(smallCity.name).toEqual("Lethbridge");
-        expect(smallCity.pop).toEqual(101500);
+        expect(lethbridge.name).toEqual("Lethbridge");
+        expect(lethbridge.pop).toEqual(101500);
     });
 
     test('show() returns display string of properties', () => {
-        expect(smallCity.show()).toEqual("Key: 4, Name: Lethbridge, Lat: -110.1, Long: 49.5, Population: 101500");
+        expect(lethbridge.show()).toEqual("Key: 4, Name: Lethbridge, Lat: 49.5, Long: -110, Population: 101500");
     });
 
     test('movedIn(value) adds value to pop', () => {
-        smallCity.movedIn(1500);
-        expect(smallCity.pop).toEqual(103000);
+        lethbridge.movedIn(1500);
+        expect(lethbridge.pop).toEqual(103000);
     });
 
     test('movedOut(value) subtracts value from pop', () => {
-        smallCity.movedOut(2999);
-        expect(smallCity.pop).toEqual(100001);
+        lethbridge.movedOut(2999);
+        expect(lethbridge.pop).toEqual(100001);
     });
 
     test('howBig() returns appropriate classification string', () => {
-        expect(smallCity.howBig()).toEqual("City");
+        expect(lethbridge.howBig()).toEqual("City");
+        expect(airdrie.howBig()).toEqual("Large Town");
+        expect(sundre.howBig()).toEqual("Town");
+        expect(caroline.howBig()).toEqual("Village");
+        expect(bearberry.howBig()).toEqual("Hamlet");
+        expect(spooky.howBig()).toEqual("Ghost Town");
     });
-
 });
 
 describe('Community Controller Testing', () => {
-    const province = new Community([{"key":1, "city":"Calgary", "lat":51.05, "long":-114.05},{"key":2, "city":"Edmonton", "lat":53.55, "long":-113.49},{"key":3, "city":"Red Deer", "lat":52.28, "long":-113.81}]);
+    const province = new Community([lethbridge, airdrie, sundre, spooky]);
 
-    test('getCity(key) returnscity by key', () => {
-        expect(province.getCity(3)).toEqual({"key":3, "city":"Red Deer", "lat":52.28, "long":-113.81});
+
+    test('getCity(key) returns city by key', () => {
+        expect(province.getCity(4)).toEqual({ "key": 4, "name": "Lethbridge", "lat": 49.5, "long": -110, "pop": 100001 });
     });
-   
+
+    test('createCity() adds new city to cityList', () => {
+        province.createCity(7, "Caroline", 53, -110.3, 500);
+        expect(province.cityList.length).toEqual(5);
+    });
+
+    test('deleteCity(key) removes city of that key', () => {
+        province.deleteCity(5);
+        expect(province.cityList.length).toEqual(4);
+        expect(province.cityList[1].name).toEqual("Sundre");
+    });
+
     test('whichSphere(city) returns appropriate hemisphere', () => {
-        expect(province.whichSphere(province.getCity(1))).toEqual("Northern Hemisphere");
+        expect(province.whichSphere(province.getCity(6))).toEqual("Northern Hemisphere");
+        expect(province.whichSphere(province.getCity(9))).toEqual("Southern Hemisphere");
     });
 
-    // test('getPopulation() returns total population of community', () => {
-    //     expect(province.getPopulation()).toEqual(600);
-    // });
+    test('getPopulation() returns total population of community', () => {
+        expect(province.getPopulation()).toEqual(103501);
+    });
 
-    // test('mostValuableAccount() returns account with highest balance', () => {
-    //     expect(province.getMostNorthern()).toEqual("");
-    // });
+    test('getMostNorthern() returns most northern city', () => {
+        expect(province.getMostNorthern().name).toEqual("Caroline");
+    });
 
-    // test('leastValuableAccount() returns account with highest balance', () => {
-    //     expect(province.getMostSouthern()).toEqual("");
-    // });
+    test('getMostSouthern() returns most southern city', () => {
+        expect(province.getMostSouthern().name).toEqual("Spooky");
+    });
+});
+
+describe('Class methods update data on server using API CRUD functionality', () => {
+
+    const url = 'http://localhost:5000/';
+
+    const serverTestData = [
+        { "key": 1, "name": "Calgary", "lat": 51.05, "long": -114.05, "pop": 1200000 },
+        { "key": 2, "name": "Edmonton", "lat": 53.55, "long": -113.49, "pop": 1000000 },
+        { "key": 3, "name": "Red Deer", "lat": 52.28, "long": -113.81, "pop": 100000 },
+    ];
+
+    const province = new Community([]);
+
+    test('loadData() loads server data into city list', async () => {
+        let data = await postData(url + 'clear'); //Does not reset on declaration..?
+        data = await postData(url + 'clear'); //Check that the server is running and clear any data
+
+        data = await postData(url + 'add', serverTestData[0]);
+        data = await postData(url + 'add', serverTestData[1]);
+        data = await postData(url + 'add', serverTestData[2]);
+
+        data = await postData(url + 'all');
+        expect(data.status).toEqual(200);
+        expect(data.length).toBe(3);
+        expect(province.cityList.length).toEqual(0);
+
+        await province.loadData(); //Load test data into cityList
+
+        expect(province.cityList.length).toEqual(3);
+        expect(province.cityList[2]).toEqual(data[2]);
+    });
+
+    test('createCity() "adds" city to server', async () => {
+        let data = await postData(url + 'all');
+        expect(data.status).toEqual(200);
+        expect(data.length).toBe(3);
+
+        await province.createCity(7, "Caroline", 53, -110.3, 500);
+
+        data = await postData(url + 'all');
+        expect(data.status).toEqual(200);
+        expect(data.length).toBe(4);
+        expect(data[3].name).toBe("Caroline");
+
+        expect(province.cityList[3]).toEqual(data[3]);
+    });
+
+    test('movedIn/Out() "updates" city pop in server', async () => {
+        let data = await postData(url + 'all');
+
+        await province.getCity(7).movedIn(600);
+        await province.getCity(7).movedOut(100);
+
+        data = await postData(url + 'all');
+        expect(data.status).toEqual(200);
+        expect(data[3].pop).toBe(1000);
+
+        expect(province.cityList[3]).toEqual(data[3]);
+    });
+
+    test('deleteCity() - deletes city in server by key', async () => {
+        let data = await postData(url + 'all');
+        expect(data.length).toBe(4);
+
+        await province.deleteCity(2);
+
+        data = await postData(url + 'all');
+        expect(data.status).toEqual(200);
+        expect(data.length).toBe(3);
+        expect(data[1].name).toBe("Red Deer");
+
+        expect(province.cityList[1]).toEqual(data[1]);
+    });
 });
