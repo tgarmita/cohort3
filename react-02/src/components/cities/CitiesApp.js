1794cb493ecd6cd180a4487a446e57ae7f96596d
unknown <tgarmita@gmail.com>
2019-12-08 16:10:44 -0700

    add server fetch load and add data

--- react-02/src/components/cities/CitiesApp.js
+++ react-02/src/components/cities/CitiesApp.js
@@ -2,7 +2,8 @@ import React, { Component } from 'react';
 import './CitiesApp.css';
 import CreateCityForm from './CreateCityForm';
 import CityPoint from './CityPoint.js';
-import { Community } from './cities.js'
+import { Community } from './cities.js';
+import serverFunctions from './serverFunctions.js'
 
 class CitiesApp extends Component {
   constructor() {
@@ -17,7 +18,22 @@ class CitiesApp extends Component {
     this.province = new Community([]);
   }
 
-  addCity = (inputs) => {
+  componentDidMount = async () => {
+    const errorMessage = await serverFunctions.loadData(this.province);
+    if (errorMessage) {
+      this.setState({
+        fetchMessage: errorMessage
+      });
+    } else if (this.province.cityList.length < 1) {
+      this.setState({
+        fetchMessage: "Warning: Server database is empty."
+      });
+    } else {
+      this.calcReport()
+    }
+  }
+
+  addCity = async (inputs) => {
     const { nameInput, latInput, longInput, popInput } = inputs;
 
     if (!nameInput) {
@@ -25,104 +41,107 @@ class CitiesApp extends Component {
         formMessage: "Please enter a city name."
       });
     } else {
-
+      this.setState({
+        formMessage: ""
+      });
       const key = this.province.getHighestKey() + 1; //change to key counter in server? key #0?
-
       const newCity = this.province.createCity(key, nameInput, latInput, longInput, popInput)
-      // const errorMessage = await serverFunctions.addData(newCity);
 
-      // if (errorMessage) {
-      //   province.deleteCity(key); // maybe copy controller and revert instead?
-      //   this.setState({
-      //     fetchMessage: errorMessage
-      //   });
-      // } else {
-
-
-    this.calcReport();
+      const errorMessage = await serverFunctions.addData(newCity);
+      if (errorMessage) {
+        // province.deleteCity(key); // maybe copy controller and revert instead?
+        this.setState({
+          fetchMessage: errorMessage
+        });
+      } else {
+        this.calcReport();
+      }
     }
   }
 
 
-removeCity = (key) => {
-  this.province.removeCity(key);
-  this.calcReport();
-}
-
-calcReport = () => {
-  this.setState({
-    totalPopulation: ""
-  });
-
-  if (this.province.cityList.length > 1) {
-    // document.getElementById("idCityReport").classList.remove("hidden");
-
-    const totalPopulationUpdate = this.province.getPopulation();
-    const mostNorthernUpdate = this.province.getMostNorthern().name;
-    const mostSouthernUpdate = this.province.getMostSouthern().name;
-
-    this.setState({
-      totalPopulation: totalPopulationUpdate,
-      mostNorthern: mostNorthernUpdate,
-      mostSouthern: mostSouthernUpdate
-    });
-  } else {
-    document.getElementById("idCityReport").classList.add("hidden");
+  removeCity = (key) => {
+    this.province.removeCity(key);
+    this.calcReport();
   }
-}
 
-renderPoints = () => {
-  return this.province.cityList.map(city => {
-    return <CityPoint key={city.key} city={city} calcReport={this.calcReport} removeCity={this.removeCity} />
-  })
-}
+  calcReport = () => {
+    this.setState({
+      totalPopulation: ""
+    });
 
-render() {
-  return (
-    <div id="idGridContainer">
-      <div id="idSummaryPanel">
-        <h2 className="subheading">Community Manager</h2>
+    if (this.province.cityList.length > 1) {
+      document.getElementById("idCityReport").classList.remove("hidden");
 
-        <CreateCityForm onSubmit={this.addCity} message={this.state.message} />
+      const mostNorth = this.province.getMostNorthern();
+      const mostSouth = this.province.getMostSouthern();
 
-        <div id="idCityReport" className="hidden">
-          <h3>Report</h3>
-          <span>Total Population: </span><span>{this.state.totalPopulation}</span><br />
-          <span>Most Northern: </span><span>{this.state.mostValuable}</span><br />
-          <span>Most Southern: </span><span>{this.state.leastValuable}</span><br />
+      const totalPopulationUpdate = this.province.getPopulation();
+      const mostNorthernUpdate = `${mostNorth.name} at ${mostNorth.lat} latitude`;
+      const mostSouthernUpdate = `${mostSouth.name} at ${mostSouth.lat} latitude`;
+      console.log(mostNorthernUpdate)
+      this.setState({
+        totalPopulation: totalPopulationUpdate,
+        mostNorthern: mostNorthernUpdate,
+        mostSouthern: mostSouthernUpdate
+      });
+    } else {
+      document.getElementById("idCityReport").classList.add("hidden");
+    }
+  }
+
+  renderPoints = () => {
+    return this.province.cityList.map(city => {
+      return <CityPoint key={city.key} city={city} calcReport={this.calcReport} removeCity={this.removeCity} />
+    })
+  }
+
+  render() {
+    return (
+      <div id="idGridContainer">
+        <div id="idSummaryPanel">
+          <h2 className="subheading">Community Manager</h2>
+
+          <CreateCityForm onSubmit={this.addCity} message={this.state.formMessage} /><br />
+
+          <div id="idCityReport" className="hidden">
+            <h3>Report</h3>
+            <span>Total Population: </span><span>{this.state.totalPopulation}</span><br />
+            <span>Most Northern: </span><span>{this.state.mostNorthern}</span><br />
+            <span>Most Southern: </span><span>{this.state.mostSouthern}</span><br />
+          </div>
         </div>
-      </div>
 
-      <div id="idMapPanel">
-        <h2>Alberta Map</h2>
-        {/* <span id="idFetchError"></span> */}
+        <div id="idMapPanel">
+          <h2>Alberta Map</h2>
+          <span id="idFetchError">{this.state.fetchMessage}</span>
 
-        <div className="map bgimg">
-          <svg id="idSVG" width="330" height="550">
-            {/* <circle cx="50" cy="50" r="7" stroke="black" stroke-width="1" fill="orange" /> */}
-            {this.renderPoints()}
-          </svg>
-        </div>
+          <div className="map bgimg">
+            <svg id="idSVG" width="330" height="550">
+              {/* <circle cx="50" cy="50" r="7" stroke="black" stroke-width="1" fill="orange" /> */}
+              {this.renderPoints()}
+            </svg>
+          </div>
 
-        <h4 id="idSelectedCity">Select City</h4>
+          <h4 id="idSelectedCity">Select City</h4>
 
-        <div id="idCityTools" className="hidden">
-          <button type='button' id="idShow">Show Details</button>
-          <button type='button' id="idHowBig">Classification</button>
-          <button type='button' id="idWhichSphere">Which Sphere?</button>
-          <button type='button' id="idDelete">Delete City</button><br />
-          <label>Pop Change:
+          <div id="idCityTools" className="hidden">
+            <button type='button' id="idShow">Show Details</button>
+            <button type='button' id="idHowBig">Classification</button>
+            <button type='button' id="idWhichSphere">Which Sphere?</button>
+            <button type='button' id="idDelete">Delete City</button><br />
+            <label>Pop Change:
                     <input id="idPopChange" type="number" min="0" />
-          </label>
-          <button type='button' id="idMovedIn">Moved In</button>
-          <button type='button' id="idMovedOut">Moved Out</button><br />
-          <span id="idCityMessage"></span>
-        </div>
+            </label>
+            <button type='button' id="idMovedIn">Moved In</button>
+            <button type='button' id="idMovedOut">Moved Out</button><br />
+            <span id="idCityMessage"></span>
+          </div>
 
+        </div>
       </div>
-    </div>
-  );
-}
+    );
+  }
 }
 
 export default CitiesApp;
--- react-02/src/components/cities/CityPoint.js
+++ react-02/src/components/cities/CityPoint.js
@@ -8,11 +8,26 @@ class CityPoint extends Component {
     }
   }
 
-  //build coordinate converter function
+  coordinateConverter = (y, x) => {
+    const lat = 275 - (y - 54.5) * 50;
+    const long = 165 + (x + 115) * 33;
+    return [lat, long];
+  }
 
   render() {
+    const [lat, long] = this.coordinateConverter(this.state.city.lat, this.state.city.long);
     return (
-      <circle cx="50" cy="50" r="7" stroke="black" strokeWidth="1" fill="orange" />
+      <>
+      <circle
+        cx={long}
+        cy={lat}
+        r="7"
+        stroke="black"
+        strokeWidth="1"
+        fill="orange" />
+
+      <text x={long + 10} y={lat - 10}>{this.state.city.name}</text>
+      </>
     );
   }
 }
